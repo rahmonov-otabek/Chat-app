cf448023b18ee6bd6bf22a7e78ae69c646274bcd
Otabek <otabekrahmonovtest@gmail.com>
2024-08-26 14:40:20 +0500

    feat: ChatController & Chat page

---
+++ resources/js/Pages/Chat/Index.vue
@@ -0,0 +1,24 @@
+<template >
+    <div class="flex">
+         <div class="w-1/2 border border-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis excepturi inventore reiciendis doloribus odio nulla alias. Similique error quisquam voluptate deserunt repudiandae autem ducimus optio sapiente maxime! Maiores, voluptates ex.
+         </div>
+         <div class="w-1/2 border border-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et laboriosam nemo dolore molestias iure, ullam quas itaque tenetur sit ducimus quia consectetur voluptates omnis nam nisi facere sunt officia architecto.
+        </div>
+    </div>
+</template>
+
+<script>
+import Main from '@/Layouts/Main.vue';
+
+export default {
+    name: 'Index',
+
+    layout: Main
+}
+</script>
+
+<style scoped>
+    
+</style>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChatController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -19,6 +20,9 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
+    Route::get('/chats', [ChatController::class, 'index'])->name('chats.index');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
